82fae1cd741335968be4fad0eaddf46290848a9d
kimjiwon-98 <jiwonpg98@gmail.com>
2021-03-15 13:37:39 +0900

    Add: login기능 및 버튼 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import firebase from "./firebase";
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import firebase from "./fbase";
+import App from "./components/App";
 
-console.log(firebase);
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
