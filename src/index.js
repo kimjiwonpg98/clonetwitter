3f0efa555a38d3b3829788c9c65e018f0209bbc4
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-03-15 10:53:40 +0900

    Add: key 숨김처리

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import firebase from "./firebase";
 import App from './App';
 
+console.log(firebase);
 ReactDOM.render(
   <React.StrictMode>
     <App />
